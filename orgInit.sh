ae7e8081fec26e7912c304d7deb346a7874d4b52
mshanemc <shane.mclaughlin@salesforce.com>
2020-01-08 08:57:50 -0600

    community boilerplate

--- orgInit.sh
+++ orgInit.sh
@@ -1,4 +1,5 @@
 sfdx force:org:create -f config/project-scratch-def.json -d 1 -s
+sfdx shane:github:package:install -g mshanemc -r community-boilerplate
 sfdx force:source:push
 sfdx force:user:permset:assign -n DemoManager
 sfdx force:org:open
